e25299859176dff4034f793c7c9135563cb4d818
Rafael7dev <biosraf@gmail.com>
2023-10-15 18:30:20 -0300

    Calculadora com react

---
+++ src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'roboto', sans-serif;
+    
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;  
+        border: 0;
+        align: right;
+
+        font-size: 24px;
+        padding: 0 10px;
+        font-family: 'roboto', sans-serif;
+        color: #ffffff;
+        
+
+}
+`
